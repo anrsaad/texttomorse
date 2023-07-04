30180e00deb4107152044c5a7da11192260a8b1b
Anrsaad <49046703+anrsaad@users.noreply.github.com>
2023-07-04 11:57:30 -0700

    Add files via upload

---
+++ morse.py
@@ -0,0 +1,61 @@
+from winsound import Beep
+
+# notes = {'C': 1635,
+#          'D': 1835,
+#          'E': 2060,
+#          'S': 1945,
+#          'F': 2183,
+#          'G': 2450,
+#          'A': 2750,
+#          'B': 3087,
+#          ' ': 37}
+
+# melodie = 'CDEFG G AAAAG AAAAG FFFFE E DDDDC'
+# for note in melodie:
+#     Beep(notes[note], 500)
+
+# core program
+# signe = 1635
+
+
+
+# block = 37
+# signe = 1835
+# bk = Beep(37, 150)
+# lp = Beep(signe, 600)
+# sp = Beep(signe, 150)
+
+# longsonund = 1835, 600
+# shortsound = 1835, 150
+# a = Beep(shortsound), Beep(longsonund), Beep(shortsound)
+# print(a)
+
+def mores():
+    a = Beep(1835, 150), Beep(1835, 600)
+    z = Beep(1835, 600), Beep(1835, 600), Beep(1835, 150), Beep(1835, 150)
+    e = Beep(1835, 150)
+    r = Beep(1835, 150), Beep(1835, 600), Beep(1835, 150)
+    t = Beep(1835, 600)
+    y = Beep(1835, 600), Beep(1835, 150), Beep(1835, 600), Beep(1835, 600)
+    u = Beep(1835, 150), Beep(1835, 150), Beep(1835, 600)
+    i = Beep(1835, 150), Beep(1835, 150)
+    o = Beep(1835, 600), Beep(1835, 600), Beep(1835, 600)
+    p = Beep(1835, 150), Beep(1835, 600), Beep(1835, 600), Beep(1835, 150)
+    q = Beep(1835, 600), Beep(1835, 600), Beep(1835, 150), Beep(1835, 600)
+    s = Beep(1835, 150), Beep(1835, 150), Beep(1835, 150)
+    d = Beep(1835, 600), Beep(1835, 150), Beep(1835, 150)
+    f = Beep(1835, 150), Beep(1835, 150), Beep(1835, 600), Beep(1835, 150)
+    g = Beep(1835, 600), Beep(1835, 600), Beep(1835, 150)
+    h = Beep(1835, 150), Beep(1835, 150), Beep(1835, 150), Beep(1835, 150)
+    j = Beep(1835, 150), Beep(1835, 600), Beep(1835, 600), Beep(1835, 600)
+    k = Beep(1835, 600), Beep(1835, 150), Beep(1835, 600)
+    l = Beep(1835, 150), Beep(1835, 600), Beep(1835, 150), Beep(1835, 150)
+    m = Beep(1835, 600), Beep(1835, 600)
+    w = Beep(1835, 150), Beep(1835, 600), Beep(1835, 600)
+    x = Beep(1835, 600), Beep(1835, 150), Beep(1835, 150), Beep(1835, 600)
+    c = Beep(1835, 600), Beep(1835, 150), Beep(1835, 600), Beep(1835, 150)
+    v = Beep(1835, 150), Beep(1835, 150), Beep(1835, 150), Beep(1835, 600)
+    b = Beep(1835, 600), Beep(1835, 150), Beep(1835, 150), Beep(1835, 150)
+    n = Beep(1835, 600), Beep(1835, 150)
+
+mores()
